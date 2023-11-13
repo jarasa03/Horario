91c92f43beefdb2d7e24d8b39ec592e7801a0340
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-11-13 18:03:10 +0100

    Add files via upload

--- Horario/JS/JavaScript.js
+++ Horario/JS/JavaScript.js
@@ -1,5 +1,5 @@
-// Lo qye hace el siguiente script es que muestra una alerta por pantalla tras haber cargado la página, no funciona con el navegador "Opera" por motivos que desconozco
+// Lo que hace el siguiente script es que muestra una alerta por pantalla tras haber cargado la página, no funciona con el navegador "Opera" por motivos que desconozco
 document.addEventListener('DOMContentLoaded', function() {
     // Tu código JavaScript aquí
-    alert('Atrapa al chihuahua para ir a un formulario');
+    alert('Atrapa a chichas para ir a un formulario');
 });
